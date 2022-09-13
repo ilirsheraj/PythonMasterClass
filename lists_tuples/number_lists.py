58bafb6d6c71e4cac582c4a1005ead0a0419c3dd
Ilir Sheraj <sherajilir@gmail.com>
2022-09-13 02:52:36 +0200

    Add files via upload

---
+++ lists_tuples/number_lists.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 13 02:48:40 2022
+
+@author: ilirsheraj
+"""
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+print(min(even))
+print(max(even))
+print()
+print(min(odd))
+print(max(odd))
+print()
+
+print(len(even))
+print(len(odd))
+
+print()
+print("mississippi".count("s"))
+print("mississippi".count("iss"))
+print("mississippi".count("issi"))
