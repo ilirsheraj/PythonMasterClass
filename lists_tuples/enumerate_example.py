d58c91c65a78d47f378755206204f14eba65976b
Ilir Sheraj <sherajilir@gmail.com>
2022-09-13 19:26:40 +0200

    Add files via upload

---
+++ lists_tuples/enumerate_example.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 13 19:08:12 2022
+
+@author: ilirsheraj
+"""
+for index, character in enumerate("abcdefgh"):
+    print(index, character)
