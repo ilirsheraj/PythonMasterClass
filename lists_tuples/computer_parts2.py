8d3d77f48aee17bd2d36f9e05e84073b512bdb6d
Ilir Sheraj <sherajilir@gmail.com>
2022-09-13 19:26:10 +0200

    Add files via upload

--- lists_tuples/computer_parts2.py
+++ lists_tuples/computer_parts2.py
@@ -9,31 +9,29 @@ available_parts = ["computer",
                    "monitor",
                    "keyboard",
                    "mouse",
-                   "mouse mat",
+#                   "mouse mat",
                    "hdmi cable",
-                   "dvd drive"
+                   "dvd drive", 
+                   "camera"
                    ]
 
-current_choice = "-"
+# valid_choices = [str(i) for i in range(1,len(available_parts) +1)]
 
+valid_choices = []
+for i in range(1, len(available_parts) + 1):
+    valid_choices.append(str(i))
+print(valid_choices)
+
+current_choice = "-"
 # Create an empty list
 computer_parts = []
 
 while current_choice != "0":
-    if current_choice in "123456":
+    if current_choice in valid_choices:
         print("Adding {}".format(current_choice))
-        if current_choice == "1":
-            computer_parts.append("computer")
-        elif current_choice == "2":
-            computer_parts.append("monitor")
-        elif current_choice == "3":
-            computer_parts.append("keyboard")
-        elif current_choice == "4":
-            computer_parts.append("mouse")
-        elif current_choice == "5":
-            computer_parts.append("mouse mat")
-        elif current_choice == "6":
-            computer_parts.append("hdmi cable")
+        index = int(current_choice) - 1
+        chosen_part = available_parts[index]
+        computer_parts.append(chosen_part)
     else:
         print("Please add options from the list below")
         for number, part in enumerate(available_parts):
