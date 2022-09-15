390a28689be52eb01ccb5b3ee50bfd486dff4a81
Ilir Sheraj <sherajilir@gmail.com>
2022-09-16 01:15:08 +0200

    Add files via upload

---
+++ lists_tuples/tuples_intro.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 16 00:51:41 2022
+
+@author: ilirsheraj
+"""
+t = "a", "b", "c"
+print(t)
+print(type(t))
+# The most common way of representing tuples
+t = ("a", "b", "c")
+print(t)
+
+name = "Tim"
+age = 10
+print(name, age, "Python", 2020)
+# print a tuple
+print((name, age, "Python", 2020))
