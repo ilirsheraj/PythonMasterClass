5f560fd1ff6c099656fc02851fa79e6067cf1190
Ilir Sheraj <sherajilir@gmail.com>
2022-09-18 23:00:31 +0200

    Add files via upload

---
+++ DictSet/dict_intro.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 18 22:44:31 2022
+
+@author: ilirsheraj
+"""
+vehicles = {
+    "dream": "Honda 250T",
+    "roadster": "BMW R1100",
+    "er5": "Kawasaki ER5",
+    "can-am": "Bomardier Can-Am 250",
+    "virago": "Yamaha XV250",
+    "tenere": "Yamaha XT650",
+    "jimny": "Suzuki Jimny 1.5",
+    "fiesta": "Ford Fiesta Ghia 1.4",
+    }
+
+
+# Index dictionaries using the key
+my_car = vehicles["fiesta"]
+print(my_car)
+print()
+
+commuter = vehicles["virago"]
+print(commuter)
+print()
+
+learner = vehicles.get("er5")
+print(learner)
+
+learner = vehicles.get("ER5")
+print(learner)
