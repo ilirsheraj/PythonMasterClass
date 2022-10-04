762c801b1dd282c583fb3cad303245e6b55e93a8
Ilir Sheraj <sherajilir@gmail.com>
2022-10-04 19:00:59 +0200

    Add files via upload

--- Modules/screen.py
+++ Modules/screen.py
@@ -48,6 +48,46 @@ radio1.grid(row=0, column=0, sticky="w")
 radio2.grid(row=2, column=0, sticky="w")
 radio3.grid(row=3, column=0, sticky="w")
 
+# Widget to display results
+resultLabel = tkinter.Label(main_window, text="Result")
+resultLabel.grid(row=2, column=2, sticky="nw")
+result = tkinter.Entry(main_window)
+result.grid(row=2, column=2,sticky="sw")
+
+# Frame for the time spinners
+timeFrame = tkinter.LabelFrame(main_window, text="Time")
+timeFrame.grid(row=3, column=0, sticky="new")
+# Time spinners
+hourSpinner = tkinter.Spinbox(timeFrame, width=2, values=tuple(range(0, 24)))
+minuteSpinner = tkinter.Spinbox(timeFrame, width=2, from_=0, to=59)
+secondSpinner = tkinter.Spinbox(timeFrame, width=2, from_=0, to=59)
+hourSpinner.grid(row=0, column=0)
+tkinter.Label(timeFrame, text=":").grid(row=0, column=1)
+minuteSpinner.grid(row=0, column=2)
+tkinter.Label(timeFrame, text=":").grid(row=0, column=3)
+secondSpinner.grid(row=0, column=4)
+timeFrame["padx"] = 36
+
+# Frame for the date spinners
+dateFrame = tkinter.Frame(main_window)
+dateFrame.grid(row=4, column=0, sticky="new")
+# Date Labels
+dayLabel = tkinter.Label(dateFrame, text="Day")
+monthLabel = tkinter.Label(dateFrame, text="Month")
+yearLabel = tkinter.Label(dateFrame, text="Year")
+dayLabel.grid(row=0, column=0, sticky="w")
+monthLabel.grid(row=0, column=1, sticky="w")
+yearLabel.grid(row=0, column=2, sticky="w")
+
+# Date Spinners
+daySpin = tkinter.Spinbox(dateFrame, width=5, from_=0, to=31)
+monthSpin = tkinter.Spinbox(dateFrame, width=5,
+							values=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"])
+yearSpin = tkinter.Spinbox(dateFrame, width=5, from_=2000, to=2099)
+daySpin.grid(row=1, column=0)
+monthSpin.grid(row=1, column=1)
+yearSpin.grid(row=1, column=2)
+
 main_window.mainloop()
 
 print(rbValue.get())
