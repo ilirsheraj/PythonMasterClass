458c0eaf73f30c05c488127c16514fc39c99327c
Ilir Sheraj <sherajilir@gmail.com>
2022-10-05 23:04:29 +0200

    Add files via upload

--- Modules/functions.py
+++ Modules/functions.py
@@ -11,10 +11,12 @@ print(python_food())
 
 
 def centre_text(text):
+	text = str(text)
 	left_margin = (80 - len(text)) // 2
 	print(" " * left_margin, text)
 
 
 centre_text("Spam and Eggs")
 centre_text("spam, spam and eggs")
+centre_text(12)
 centre_text("spam, spam, spam and spam")
