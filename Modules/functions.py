349505d09f5e5fb04e6cc813ba81ecef21da82fd
Ilir Sheraj <sherajilir@gmail.com>
2022-10-05 22:48:02 +0200

    Add files via upload

---
+++ Modules/functions.py
@@ -0,0 +1,20 @@
+def python_food():
+	width = 80
+	text = "Spam and Eggs"
+	left_margin = (width - len(text)) // 2
+	print(" " * left_margin, text)
+
+
+# Call the function
+# Since function has no return, it returns None
+print(python_food())
+
+
+def centre_text(text):
+	left_margin = (80 - len(text)) // 2
+	print(" " * left_margin, text)
+
+
+centre_text("Spam and Eggs")
+centre_text("spam, spam and eggs")
+centre_text("spam, spam, spam and spam")
