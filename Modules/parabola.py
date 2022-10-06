5ff670e86f7c62dba1ba7c38f44f8daa3a28f0fe
Ilir Sheraj <sherajilir@gmail.com>
2022-10-06 02:08:02 +0200

    Add files via upload

--- Modules/parabola.py
+++ Modules/parabola.py
@@ -22,6 +22,7 @@ def draw_axis(canvas):
 	canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
 	canvas.create_line(-x_origin, 0, x_origin, 0, fill="black")
 	canvas.create_line(0, y_origin, 0, -y_origin, fill="black")
+	print(locals())
 
 
 def plot(canvas, x, y):
@@ -32,10 +33,16 @@ mainWindow = tkinter.Tk()
 mainWindow.title("Parabola")
 mainWindow.geometry("640x480")
 
-canvas = tkinter.Canvas(mainWindow, width=640, height=480)
+canvas = tkinter.Canvas(mainWindow, width=320, height=480)
 canvas.grid(row=0, column=0)
 
+canvas2 = tkinter.Canvas(mainWindow, width=320, height=480, background="blue")
+canvas2.grid(row=0, column=1)
+
+print(repr(canvas))
+print(repr(canvas2))
 draw_axis(canvas)
+draw_axis(canvas2)
 
 for x in range(-100, 101):
 	y = parabola(x)
