7f5234bb19d9d6c99dbb4b4e0328313d631417a7
Ilir Sheraj <sherajilir@gmail.com>
2022-10-09 01:39:15 +0200

    Add files via upload

--- OOP/oop_intro.py
+++ OOP/oop_intro.py
@@ -6,6 +6,9 @@ class Kettle(object):
 		self.price = price
 		self.on = False
 
+	def switch_on(self):
+		self.on = True
+
 
 # Create the first instance
 kenwood = Kettle("Kenwood", 8.99)
@@ -23,3 +26,22 @@ print(hamilton.make)
 print(hamilton.price)
 
 print("Models: {} = {}, {} = {}".format(kenwood.make, kenwood.price, hamilton.make, hamilton.price))
+print()
+print("Models: {0.make} = {0.price}, {1.make} = {1.price}".format(kenwood, hamilton))
+
+print(hamilton.on)
+hamilton.switch_on()
+print(hamilton.on)
+
+# This is another way of calling the class definition initially
+Kettle.switch_on(kenwood)
+print(kenwood.on)
+kenwood.switch_on()
+print("-"*30)
+
+# Classes are dynamic and can be edited.
+# Create a new attribute for kenwood but not for hamilton and see the difference
+kenwood.power = 1.5
+print(kenwood.power)
+# hamilton.power will give error because it does not have any such attribute
+print(hamilton.power)
