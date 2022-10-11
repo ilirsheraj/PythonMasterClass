f01dcb036c17b503fffed63b8825f36d6efa6cde
Ilir Sheraj <sherajilir@gmail.com>
2022-10-12 00:34:32 +0200

    Add files via upload

--- OOP/inherit.py
+++ OOP/inherit.py
@@ -7,12 +7,23 @@ class Person:
 		print(self.firstname, self.lastname)
 
 
+class Lawyers(Person):
+	"""
+	def __init__(self, fname, lname) overwrites parent class
+	and it uses it's own method and attributes
+	"""
+
+	def __init__(self, fname, lname):
+		Person.__init__(self, fname, lname)
+		# self.firstname = fname
+		# self.lastname = lname
+
+	def printinfo(self):
+		print(self.firstname, self.lastname)
+
+
 florist = Person("Jane", "Flowers")
 florist.print_name()
-
-class Lawyers(Person):
-	# Instantiate the child class
-	pass
-
 happy_lawyers = Lawyers("Jack", "Smiley")
 happy_lawyers.print_name()
+happy_lawyers.printinfo()
