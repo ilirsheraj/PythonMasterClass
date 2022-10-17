160929a7c985f7189be76a853feb7584d82c30e0
Ilir Sheraj <sherajilir@gmail.com>
2022-10-17 22:53:37 +0200

    Add files via upload

--- OOP/player.py
+++ OOP/player.py
@@ -4,7 +4,7 @@ class Player(object):
 		self.name = name
 		self._lives = 3
 		self._level = 1
-		self.score = 0
+		self._score = 0
 
 	def _get_lives(self):
 		return self._lives
@@ -22,13 +22,22 @@ class Player(object):
 	def _set_level(self, level):
 		if level > 0:
 			delta = level - self._level
-			self.score += delta * 1000
+			self._score += delta * 1000
 			self._level = level
 		else:
 			print("Level can't be less than 1")
 
 	lives = property(_get_lives, _set_lives)
 	level = property(_get_level, _set_level)
+
+	# Alternative syntax using decorators
+	@property
+	def score(self):
+		return self._score
+
+	@score.setter
+	def score(self, score):
+		self._score = score
 
 	def __str__(self):
 		""" A function to print out the values of the attributes """
