4bb3ba2ff67a3b24834f0bc4b6d0d63928453197
Ilir Sheraj <sherajilir@gmail.com>
2022-10-18 22:37:56 +0200

    Add files via upload

--- OOP/html_doc.py
+++ OOP/html_doc.py
@@ -8,8 +8,8 @@ class Tag(object):
 	def __str__(self):
 		return "{0.start_tag}{0.contents}{0.end_tag}".format(self)
 
-	def display(self):
-		print(self)
+	def display(self, file=None):
+		print(self, file=file)
 
 
 class DocType(Tag):
@@ -35,8 +35,35 @@ class Body(Tag):
 		new_tag = Tag(name, contents)
 		self._body_contents.append(new_tag)
 
-	def display(self):
+	def display(self, file=None):
 		for tag in self._body_contents:
 			self.contents += str(tag)
 
-		super().display()
+		super().display(file=file)
+
+
+class HtmlDoc(object):
+
+	def __init__(self):
+		self._doc_type = DocType()
+		self._head = Head()
+		self._body = Body()
+
+	def add_tag(self, name, contents):
+		self._body.add_tag(name, contents)
+
+	def display(self, file=None):
+		self._doc_type.display(file=file)
+		print("<html>", file=file)
+		self._head.display(file=file)
+		self._body.display(file=file)
+		print("<html>", file=file)
+
+
+if __name__ == "__main__":
+	my_page = HtmlDoc()
+	my_page.add_tag("h1", "Main Heading")
+	my_page.add_tag("h2", "sub-heading")
+	my_page.add_tag("p", "This is a paragraph that will appear in the page")
+	with open("test.html", "w") as test_doc:
+		my_page.display(file=test_doc)
