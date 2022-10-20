4f18ceb6aa9fdeff771b5ae07404984d9c6b7315
Ilir Sheraj <sherajilir@gmail.com>
2022-10-20 22:31:34 +0200

    Add files via upload

--- Databases/long_commands.sql
+++ Databases/long_commands.sql
@@ -25,14 +25,24 @@ INNER JOIN artists ON albums.artist = artists._id
 WHERE artists.name LIKE "%jefferson%"
 ORDER BY artists.name, albums.name, songs.track;
 
--- View
+-- Create a View of the Table
 CREATE VIEW artist_list AS
 SELECT artists.name, albums.name, songs.track, songs.title FROM songs
 INNER JOIN albums ON songs.album = albums._id
 INNER JOIN artists ON albums.artist = artists._id
 ORDER BY artists.name, albums.name, songs.track;
 .schema
-SELECT FROM artist_list;
+SELECT * FROM artist_list;
 
+-- To close the view use the following command
+DROP VIEW artist_list;
 
+-- Housekeeping
+DELETE FROM songs WHERE track < 50;
+SELECT * FROM songs;
+SELECT * FROM artist_list;
+SELECT * FROM songs WHERE track <> 71;
+SELECT COUNT(*) FROM songs;
+SELECT COUNT(*) FROM albums;
+SELECT COUNT(*) FROM artists;
 
