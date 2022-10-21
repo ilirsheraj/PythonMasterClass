2651373eaee7c6499f48d10fddec002c54b0a17a
Ilir Sheraj <sherajilir@gmail.com>
2022-10-21 02:05:27 +0200

    Add files via upload

---
+++ Databases/contacts.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+# Create a database
+db = sqlite3.connect("contacts.sqlite")
+# create a table
+db.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)")
+db.execute("INSERT INTO contacts(name, phone, email) VALUES('Tim', 46966, 'tim@email.com')")
+db.execute("INSERT INTO contacts VALUES('Brian', 1234, 'brian@email.com')")
+
+# Query the DB in python
+# No need to add semicolon at the end
+cursor = db.cursor()
+cursor.execute("SELECT * FROM contacts")
+
+# print(cursor.fetchall())  # returns a list of tuples
+print(cursor.fetchone())  # first row
+print(cursor.fetchone())  # second row
+print(cursor.fetchone())  # None, there ar eonly two rows
+
+# In this case this loop will not print anything
+for name, phone, email in cursor:
+	print(name)
+	print(phone)
+	print(email)
+	print("-" * 20)
+
+cursor.close()
+db.close()
+
+
+
