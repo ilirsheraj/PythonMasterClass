4f27280744880807fd5cb9b83ef4b0fd12522448
Ilir Sheraj <sherajilir@gmail.com>
2022-10-21 23:56:28 +0200

    Add files via upload

--- Databases/contacts_2.py
+++ Databases/contacts_2.py
@@ -6,13 +6,21 @@ db = sqlite3.connect("contacts.sqlite")
 # for row in db.execute("SELECT * FROM contacts"):
 # 	print(row)
 
-# Update the database specific for phone number 1234
-# update_sql = "UPDATE contacts SET email = 'update@update.com' WHERE contacts.phone = 1234"
+new_email = "newemail@update.com"
+# phone = 1234
 
-# To change all emails
-update_sql = "UPDATE contacts SET email = 'update@update.com'"
+# To mak ethe phone customizable
+phone = input("Please enter the phone number: ")
+
+# Update the database specific for phone number 1234
+update_sql = "UPDATE contacts SET email = '{}' WHERE phone = {}".format(new_email, phone)
+print(update_sql)
+
+# # To change all emails
+# update_sql = "UPDATE contacts SET email = 'update@update.com'"
 update_cursor = db.cursor()
-update_cursor.execute(update_sql)
+# update_cursor.execute(update_sql)
+update_cursor.executescript(update_sql)
 print("{} rows updated".format(update_cursor.rowcount))
 
 print()
