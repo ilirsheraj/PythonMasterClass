eaf3b0c06d1e01ac0441fa3adfcee1be1a09e985
Ilir Sheraj <sherajilir@gmail.com>
2022-10-22 01:16:56 +0200

    Add files via upload

--- Databases/contacts_challenge.py
+++ Databases/contacts_challenge.py
@@ -5,7 +5,19 @@ import sqlite3
 # Create a database
 conn = sqlite3.connect("contacts.sqlite")
 
-for row in conn.execute("SELECT * FROM contacts"):
+# # for row in conn.execute("SELECT * FROM contacts"):
+# for row in conn.execute("SELECT * FROM sqlite_master"):
+# 	print(row)
+#
+# conn.close()
+
+# Challenge 2
+person = input("Please enter a name: ")
+
+# Pass a tuple
+# for row in conn.execute("SELECT * FROM contacts WHERE name = ?", (person,)):
+# To make it case insensitive use `LIKE`
+for row in conn.execute("SELECT * FROM contacts WHERE name LIKE ?", (person,)):
 	print(row)
 
 conn.close()
