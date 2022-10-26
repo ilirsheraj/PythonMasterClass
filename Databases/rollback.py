64ddb595734b98700bc0e37be71b9019ce743bc1
Ilir Sheraj <sherajilir@gmail.com>
2022-10-27 01:44:55 +0200

    Add files via upload

--- Databases/rollback.py
+++ Databases/rollback.py
@@ -1,10 +1,27 @@
+import sqlite3
+
+db = sqlite3.connect("accounts.sqlite")
+db.execute("CREATE TABLE IF NOT EXISTS accounts (name TEXT PRIMARY KEY NOT NULL, balance INTEGER NOT NULL)")
+db.execute("CREATE TABLE IF NOT EXISTS transactions (time TIMESTAMP NOT NULL, account TEXT NOT NULL, "
+		   "amount INTEGER NOT NULL, PRIMARY KEY (time, account))")
+
+
 class Account(object):
 
 	def __init__(self, name: str, opening_balance: int = 0):
-		self.name = name
-		self._balance = opening_balance
-		print("Account created for {}. ".format(self.name), end='')
-		self.show_balance()  # define below
+		cursor = db.execute("SELECT name, balance FROM accounts WHERE (name = ?)", (name,))
+		row = cursor.fetchone()
+
+		if row:
+			self.name, self._balance = row
+			print("Retrieved record for {}".format(self.name), end='')
+		else:
+			self.name = name
+			self._balance = opening_balance
+			cursor.execute("INSERT INTO accounts VALUES(?, ?)", (name, opening_balance))
+			cursor.connection.commit()
+			print("Account created for {}".format(self.name), (name,))
+		self.show_balance()
 
 	def deposit(self, amount: int) -> float:
 		if amount > 0.0:
@@ -33,3 +50,9 @@ if __name__ == "__main__":
 	john.withdraw(30)
 	john.withdraw(0)
 	john.show_balance()
+
+	terry = Account("Terry")
+	graham = Account("Graham", 9000)
+	eric = Account("Eric", 7000)
+
+	db.close()
