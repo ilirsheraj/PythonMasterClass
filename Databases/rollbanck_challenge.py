7fc697f1ae188db22298914a74a1a958d1ce142e
Ilir Sheraj <sherajilir@gmail.com>
2022-10-28 00:25:08 +0200

    Add files via upload

--- Databases/rollbanck_challenge.py
+++ Databases/rollbanck_challenge.py
@@ -1,11 +1,14 @@
 import datetime
 import sqlite3
 import pytz
+import pickle
+
 
 db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
 db.execute("CREATE TABLE IF NOT EXISTS accounts (name TEXT PRIMARY KEY NOT NULL, balance INTEGER NOT NULL)")
-db.execute("CREATE TABLE IF NOT EXISTS history (time TIMESTAMP NOT NULL, account TEXT NOT NULL, "
-		   "amount INTEGER NOT NULL, PRIMARY KEY (time, account))")
+db.execute("CREATE TABLE IF NOT EXISTS history (time TIMESTAMP NOT NULL,"
+		   "account TEXT NOT NULL, amount INTEGER NOT NULL,"
+		   "zone INTEGER NOT NULL, PRIMARY KEY (time, account))")
 db.execute("CREATE VIEW IF NOT EXISTS localhistory AS "
 		   "SELECT strftime('%Y-%m-%d %H:%M:%f', history.time, 'localtime') AS localtime,"
 		   "history.account, history.amount FROM history ORDER BY history.time")
@@ -15,9 +18,13 @@ class Account(object):
 
 	@staticmethod
 	def _current_time():
-		return pytz.utc.localize(datetime.datetime.utcnow())
+		# return pytz.utc.localize(datetime.datetime.utcnow())
 		# local_time = pytz.utc.localize(datetime.datetime.utcnow())
 		# return local_time.astimezone()
+		utc_time = pytz.utc.localize(datetime.datetime.utcnow())
+		local_time = utc_time.astimezone()
+		zone = local_time.tzinfo
+		return utc_time, zone
 
 	def __init__(self, name: str, opening_balance: int = 0):
 		cursor = db.execute("SELECT name, balance FROM accounts WHERE (name = ?)", (name,))
@@ -36,9 +43,10 @@ class Account(object):
 
 	def _save_update(self, amount):
 		new_balance = self._balance + amount
-		deposit_time = Account._current_time()
+		deposit_time, zone = Account._current_time()  # unpacking returned tuple
+		picked_zone = pickle.dumps(zone)
 		db.execute("UPDATE accounts SET balance = ? WHERE (name = ?)", (new_balance, self.name))
-		db.execute("INSERT into history VALUES(?, ?, ?)", (deposit_time, self.name, amount))
+		db.execute("INSERT into history VALUES(?, ?, ?, ?)", (deposit_time, self.name, amount, picked_zone))
 		db.commit()
 		self._balance = new_balance
 
