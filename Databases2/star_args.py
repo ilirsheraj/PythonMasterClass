e4eb85e85e28b92a1748005a59a8cb3a32281b45
Ilir Sheraj <sherajilir@gmail.com>
2022-10-28 21:27:57 +0200

    Add files via upload

--- Databases2/star_args.py
+++ Databases2/star_args.py
@@ -11,14 +11,45 @@ def average(*args):
 	return mean / len(args)
 
 
-print(average(1, 2, 3, 4))
-print()
-
-
 def build_tuple(*args):
 	return args
 
 
+def word_length(*args):
+	char_count = 0
+	for i in args:
+		char_count += len(i)
+	return char_count / len(args)
+
+
+def print_backwards(*args):
+	for i in args[::-1]:
+		print(i[::-1], end=' ')
+
+
+def smallest(*args):
+	return min(args)
+
+
+print(average(1, 2, 3, 4))
+print("-" * 30)
+
 message = build_tuple("hello", "planet", "earth", "take", "me", "to", "your", "leader")
 print(type(message))
 print(message)
+print("-" * 30)
+
+number_tuple = build_tuple(1, 2, 3, 4, 5, 6)
+print(type(number_tuple))
+print(number_tuple)
+print("-" * 30)
+
+count = word_length("hello", "planet", "earth", "take", "me", "to", "your", "leader")
+print("The average word length is {}".format(count))
+print("-" * 30)
+
+lowest = smallest(9, 4, 56, 1, 5, -23, 84)
+print(smallest(lowest))
+print("-" * 30)
+
+print_backwards("hello", "planet", "earth", "take", "me", "to", "your", "leader")
