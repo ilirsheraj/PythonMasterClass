a62dc3039c583e4aaf6aa8453cf8718deaf97ea8
Ilir Sheraj <sherajilir@gmail.com>
2022-10-28 16:35:54 +0200

    Add files via upload

--- Databases/checkdb_challenge.py
+++ Databases/checkdb_challenge.py
@@ -10,8 +10,10 @@ db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
 
 for row in db.execute("SELECT * FROM history"):
 	utc_time = row[0]
-	pickled_zone = row[3]
-	zone = pickle.loads(pickled_zone)
+	# pickled_zone = row[3]
+	# zone = pickle.loads(pickled_zone)
+	# zone = pytz.timezone("Australia/Adelaide")
+	zone = pytz.timezone("CET")
 	local_time = pytz.utc.localize(utc_time).astimezone(zone)
 	print("{}\t{}\t{}".format(utc_time, local_time, local_time.tzinfo))
 
