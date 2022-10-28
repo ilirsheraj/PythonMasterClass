a49f3de474793c6beea28e6c959338cc12f8c103
Ilir Sheraj <sherajilir@gmail.com>
2022-10-28 19:53:45 +0200

    Add files via upload

--- Databases2/jukebox.py
+++ Databases2/jukebox.py
@@ -30,11 +30,20 @@ artistList = tkinter.Listbox(mainWindow)
 artistList.grid(row=1, column=0, sticky="nsew", rowspan=2, padx=(30,0))
 artistList.config(border=2, relief="sunken")
 
+artistScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=artistList.yview)
+artistScroll.grid(row=1, column=0, sticky="nse", rowspan=2)
+artistList['yscrollcommand'] = artistScroll.set
+
 # ==== Album Listbox ====
 albumLV = tkinter.Variable(mainWindow)
 albumLV.set(("Choose ann Artist",))
 albumList = tkinter.Listbox(mainWindow, listvariable=albumLV)
 albumList.grid(row=1, column=1, sticky="nsew", padx=(30,0))
+albumList.config(border=2, relief="sunken")
+
+albumScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=albumList.yview)
+albumScroll.grid(row=1, column=1, sticky="nse", rowspan=2)
+albumScroll['yscrollcommand'] = albumScroll.set
 
 # ==== Song Listbox ====
 songLV = tkinter.Variable(mainWindow)
@@ -43,7 +52,13 @@ songList = tkinter.Listbox(mainWindow, listvariable=songLV)
 songList.grid(row=1, column=2, sticky="nsew", padx=(30,0))
 songList.config(border=2, relief="sunken")
 
+# songScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=songList.yview)
+# songScroll.grid(row=1, column=2, sticky="nse", rowspan=2)
+# songScroll["yscrollcommand"] = songScroll.set
+
 # ==== Main Loop ====
+testList = range(0, 100)
+albumLV.set(testList)
 mainWindow.mainloop()
 print("Closing the Database Connection")
 conn.close()
