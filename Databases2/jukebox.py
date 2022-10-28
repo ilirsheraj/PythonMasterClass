99e428a052cce2e222772160cf27bc1bf1f32ff1
Ilir Sheraj <sherajilir@gmail.com>
2022-10-29 01:37:33 +0200

    Add files via upload

--- Databases2/jukebox.py
+++ Databases2/jukebox.py
@@ -18,6 +18,19 @@ class Scrollbox(tkinter.Listbox):
 		self['yscrollcommand'] = self.scrollbar.set
 
 
+def get_albums(event):
+	lb = event.widget
+	index = lb.curselection()[0]
+	artist_name = lb.get(index),  # This is a tuple
+
+	# Get the artist ID from the database row
+	artist_id = conn.execute("SELECT artists._id FROM artists WHERE artists.name=?", artist_name).fetchone()
+	alist = []
+	for row in conn.execute("SELECT albums.name FROM albums WHERE albums.artist =? ORDER BY albums.name", artist_id):
+		alist.append(row[0])
+	albumLV.set(tuple(alist))
+
+
 mainWindow = tkinter.Tk()
 mainWindow.title("Music DB Browser")
 mainWindow.geometry("1024x768")
@@ -46,6 +59,8 @@ artistList.config(border=2, relief="sunken")
 
 for artist in conn.execute("SELECT artists.name FROM artists ORDER BY artists.name"):
 	artistList.insert(tkinter.END, artist[0])
+
+artistList.bind('<<ListboxSelect>>', get_albums)
 
 # artistScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=artistList.yview)
 # artistScroll.grid(row=1, column=0, sticky="nse", rowspan=2)
