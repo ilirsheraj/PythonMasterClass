6216a0996054de0a45acc506c1168a3c75850cc0
Like <sherajilir@gmail.com>
2022-10-30 22:46:19 +0100

    Add files via upload

--- Gen_Comprh/listfor.py
+++ Gen_Comprh/listfor.py
@@ -1,9 +1,14 @@
 print(__file__)
 
 numbers = [1, 2, 3, 4, 5, 6]
+
+number = int(input("Please enter a number, and I'll tell you it's square: "))
 squares = []
 for number in numbers:
 	squares.append(number ** 2)
 print(squares)
 
-# Do the same using list comprehension
+index_pos = numbers.index(number)
+print(squares[index_pos])
+
+# number has the value of the last number in the loop!
