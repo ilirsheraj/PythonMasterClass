b62eb774e4ded94c3cf632eb95dbfb941f37a437
Like <sherajilir@gmail.com>
2022-10-30 22:46:36 +0100

    Add files via upload

--- Gen_Comprh/listcomp.py
+++ Gen_Comprh/listcomp.py
@@ -2,6 +2,12 @@ print(__file__)
 
 numbers = [1, 2, 3, 4, 5, 6]
 
+number = int(input("Please enter a number, and I'll tell you it's square: "))
+
 # squares = [number ** 2 for number in numbers]
 squares = [number ** 2 for number in range(1,7)]
 print(squares)
+
+index_pos = numbers.index(number)
+print(index_pos)
+print(squares[index_pos])
