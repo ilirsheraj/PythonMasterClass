3b7b9d3341caaa3c35639b52ed4b4c0971f4e6aa
Like <sherajilir@gmail.com>
2022-10-31 22:39:42 +0100

    Add files via upload

--- Gen_Comprh/conditional_compr.py
+++ Gen_Comprh/conditional_compr.py
@@ -14,8 +14,18 @@ meals = []
 for meal in menu:
 	if "spam" not in meal:
 		meals.append(meal)
-		
+	else:
+		meals.append("a meal was skipped")
 print(meals)
 
-meals = [meal for meal in menu if "spam" not in meal]
+# meals = [meal for meal in menu if "spam" not in meal]
+# meals = [meal for meal in menu if "spam" not in meal if "chicken" not in meal]
+meals = [meal for meal in menu if "spam" not in meal and "chicken" not in meal]
 print(meals)
+
+fussy_meal = [meal for meal in menu if "spam" in meal or "eggs" in meal if not ("bacon" in meal and "sausage" in meal)]
+print(fussy_meal)
+
+fussy_meal = [meal for meal in menu if
+			  ("spam" in meal or "eggs" in meal) and not ("bacon" in meal and "sausage" in meal)]
+print(fussy_meal)
