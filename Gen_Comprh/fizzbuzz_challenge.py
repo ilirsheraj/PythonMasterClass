78168ae9479990c87791f3cd5772568a87cf78ef
Like <sherajilir@gmail.com>
2022-11-01 15:09:57 +0100

    Add files via upload

--- Gen_Comprh/fizzbuzz_challenge.py
+++ Gen_Comprh/fizzbuzz_challenge.py
@@ -1,8 +1,6 @@
 # Create a new Python file - I've called this one "fizzbuzz.py".
-#
 # Use the conditional expression from the previous video to produce
 # a list comprehension that returns the fizzbuzz results.
-#
 # If a number's divisible by 3, the value should be "fizz".
 # If it's divisible by 5, the value should be "buzz".
 # If it's divisible by both 3 and 5, the value should be "fizz buzz"
@@ -13,4 +11,8 @@
 for x in range(1, 31):
 	fizzbuzz = "fizz buzz" if x % 15 == 0 else "fizz" if x % 3 == 0 else "buzz" if x % 5 == 0 else str(x)
 	print(fizzbuzz)
-	
+
+x = range(1, 31)
+fizzbuzz = ["fiz buz" if number % 15 == 0 else "fizz" if number % 3 == 0 else "buzz" if number % 5 == 0 else str(number)
+			for number in x]
+print(fizzbuzz)
