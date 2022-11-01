576d47dd2ac04a2067f30e23ab5f721afcf1286e
Like <sherajilir@gmail.com>
2022-11-01 22:19:25 +0100

    Add files via upload

--- Gen_Comprh/challenge2-challenges.py
+++ Gen_Comprh/challenge2-challenges.py
@@ -1,21 +1,17 @@
 # Create a comprehension that returns a list of all the locations that have an exit to the forest.
 # The list should contain the description of each location, if it's possible to get to the forest from there.
-#
 # The forest is location 5 in the locations dictionary
 # The exits for each location are represented by the exits dictionary.
-#
 # Remember that a dictionary has a .values() method, to return a list of the values.
-#
 # The forest can be reached from the road, and the hill; so those should be the descriptions that appear in your list.
 #
 # Test your program with different destinations (such as 1 for the road) to make sure it works.
-#
 # Once it's working, modify the program so that the comprehension returns a list of tuples.
 # Each tuple consists of the location number and the description.
 #
 # Finally, wrap your comprehension in a for loop, and print the lists of all the locations that lead to each of the
-# other locations in turn.
-# In other words, use a for loop to run the comprehension for each of the keys in the locations dictionary.
+# other locations in turn. In other words, use a for loop to run the comprehension
+# for each of the keys in the locations dictionary.
  
  
 locations = {0: "You are sitting in front of a computer learning Python",
@@ -31,3 +27,13 @@ exits = {0: {"Q": 0},
          3: {"W": 1, "Q": 0},
          4: {"N": 1, "W": 2, "Q": 0},
          5: {"W": 2, "S": 1, "Q": 0}}
+
+loc = 1
+forest = [locations[xit] for xit in exits if loc in exits[xit].values()]
+print(forest)
+print("=" * 80)
+
+for loc in sorted(locations):
+    forest = [(exit, locations[exit]) for exit in exits if loc in exits[exit].values()]
+    print("Locations leading to {}".format(loc), end = "\t")
+    print(forest)
