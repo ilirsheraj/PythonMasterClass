cef3b13a543cac55f8c8291f999869d3fce5fc73
Like <sherajilir@gmail.com>
2022-11-02 16:54:37 +0100

    Add files via upload

--- Gen_Comprh/map_intro.py
+++ Gen_Comprh/map_intro.py
@@ -1,6 +1,3 @@
-import timeit
-
-
 text = "what have the romans done for us"
 
 # capitals = [char.upper() for char in text]
@@ -19,6 +16,7 @@ text = "what have the romans done for us"
 #
 # for x in map(str.upper, text.split(" ")):
 # 	print(x)
+import timeit
 
 
 def comp_caps():
@@ -48,3 +46,14 @@ if __name__ == "__main__":
 	print(map_caps())
 	print(comp_words())
 	print(map_words())
+
+	# print(timeit.timeit("comp_caps()", setup="from map_intro import comp_caps", number=10000))
+	# print(timeit.timeit("map_caps()", setup="from map_intro import map_caps", number=10000))
+	# print(timeit.timeit("comp_words()", setup="from map_intro import comp_words", number=10000))
+	# print(timeit.timeit("map_words()", setup="from map_intro import map_words", number=10000))
+
+	# Another way
+	print(timeit.timeit(comp_caps, number=100000))
+	print(timeit.timeit(map_caps, number=100000))
+	print(timeit.timeit(comp_words, number=100000))
+	print(timeit.timeit(map_words, number=100000))
