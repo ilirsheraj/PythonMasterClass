3e9914fb377488100ccdd79cd895299b493f4805
Like <sherajilir@gmail.com>
2022-11-03 23:30:04 +0100

    Add files via upload

---
+++ Mixed/open_url.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+
+# webbrowser.open("https://docs.python.org/3.10/library/webbrowser")
+
+# Open in google chrome
+chrome = webbrowser.get(using="google-chrome")
+chrome.open("https://learnprogramming.academy/courses")
