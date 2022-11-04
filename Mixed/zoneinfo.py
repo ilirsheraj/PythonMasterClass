b6936781ad743b1eb779a926a402832a6fc159a4
Like <sherajilir@gmail.com>
2022-11-04 21:16:29 +0100

    Add files via upload

--- Mixed/zoneinfo.py
+++ Mixed/zoneinfo.py
@@ -8,6 +8,18 @@ print(utc_now)
 local_now = utc_now.astimezone()
 print(local_now)
 
-new_york_tz = zoneinfo.ZoneInfo("America/New_York")
+new_york_tz = datetime.ZoneInfo("America/New_York")
 ny_now = utc_now.astimezone(tz=new_york_tz)
 print(ny_now)
+
+france_tz = zoneinfo.ZoneInfo("Europe/Paris")
+france_now = utc_now.astimezone(france_tz)
+print(france_now)
+
+# Return all the available keys
+print()
+print("Available timezone keys")
+print("-" * 23)
+
+for zone_key in sorted(zoneinfo.available_timezones()):
+	print(zone_key)
